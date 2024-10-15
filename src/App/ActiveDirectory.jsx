2d544dd1e9eaefc84d781a9a6c863a5d54f9986f
Jeamie <jeamie08@gmail.com>
2024-10-16 02:11:57 +0800

    Copy button on AD

--- src/App/ActiveDirectory.jsx
+++ src/App/ActiveDirectory.jsx
@@ -3,16 +3,17 @@ import CopyButton from "../components/CopyButton";
 import "./apps.css";
 
 function ActiveDirectory({ employee }) {
-  const copyAD = `Fullname: ${employee.firstName} ${employee.lastName}\nEmail: ${employee.emailAdd}\nADID: ${employee.adid}\nJob Title: ${employee.jobTitle}\nDepartment: ${employee.department}\nManager: ${employee.manager}\nManager: ${employee.manager}\nAddress: ${employee.address}\nZip Code: ${employee.zipCode}\nCountry: ${employee.country}`;
+  const copyAD = `Fullname: ${employee.firstName} ${employee.lastName}\nEmail: ${employee.emailAdd}\nADID: ${employee.adid}\nJob Title: ${employee.jobTitle}\nDepartment: ${employee.department}\nManager: ${employee.manager}\nAddress: ${employee.address}\nZip Code: ${employee.zipCode}\nCountry: ${employee.country}`;
   return (
     <div className="emp-task">
-      {copyAD.split("\n").map((line, index) => (
-        <span key={index}>
-          {line}
-          {index < copyAD.split("\n").length - 1 && <br />}
-        </span>
-      ))}
-
+      <div className="ad-emp-details">
+        {copyAD.split("\n").map((line, index) => (
+          <span key={index}>
+            {line}
+            {index < copyAD.split("\n").length - 1 && <br />}
+          </span>
+        ))}
+      </div>
       <div>
         <CopyButton text={copyAD} />
       </div>
