8a2b0bd91304b667588af70a42dc5ef513227a6c
Jeamie <jeamie08@gmail.com>
2024-10-17 03:03:56 +0800

    Added the copy to clipboard feature in all the pages

--- src/Utils/CopyToClipboard.jsx
+++ src/Utils/CopyToClipboard.jsx
@@ -1,11 +1,26 @@
-// clipboardUtils.js
-export const copyToClipboard = (text) => {
-  navigator.clipboard
-    .writeText(text)
-    .then(() => {
-      console.log("Copied to clipboard:", text);
-    })
-    .catch((err) => {
+// useClipboard.js
+import { useState } from "react";
+
+const useClipboard = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const copyToClipboard = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      showNotification();
+    } catch (err) {
       console.error("Failed to copy: ", err);
-    });
+    }
+  };
+
+  const showNotification = () => {
+    setIsVisible(true);
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 3000); // Notification duration
+  };
+
+  return { copyToClipboard, isVisible };
 };
+
+export default useClipboard;
